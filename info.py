326968f1ef8164106aefafd0f43a31ba2e1fcb0c
Lucif45 <luciferoiss0990@gmail.com>
2024-10-24 15:31:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '25271033'))
 API_HASH = environ.get('API_HASH', '52c20a82e2e0d649ce9edc215e534791')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7870551266:AAHYDgT96a3sRvyJGZfiklXmNhHZVDiVg1E")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -24,7 +24,7 @@ URL = environ.get("URL", "")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002295166225'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6361728798 7816710024').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6361728798').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://luciferoiss0990:L2DkEDlVQxKrzjGV@cluster0.ekdhp.mongodb.net/?retryWrites=true&w=majority")
